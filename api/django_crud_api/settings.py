c439f8688ff6921535ee13152897afa7162d8f2b
DiegoHrz <hernandezwdiego@gmail.com>
2024-12-29 21:38:46 -0500

    Add django rest frameworking config

--- api/django_crud_api/settings.py
+++ api/django_crud_api/settings.py
@@ -37,10 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tasks'
+    'corsheaders',
+    'rest_framework',
+    'tasks',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +52,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',  # URL del frontend en desarrollo
+]
+
 
 ROOT_URLCONF = 'django_crud_api.urls'
 
